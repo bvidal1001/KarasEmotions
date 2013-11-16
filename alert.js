9331368905ab58de6761d71eb5bd624af166e6b5
bvidal1001 <bradjvidal@yahoo.com>
2013-11-16 10:04:53 -0800

    style

--- alert.js
+++ alert.js
@@ -1,1 +1,13 @@
+
+	var header = document.getElementsByTagName('header')[0];
+	header.style.backgroundColor = "#FFCCFF";
+
+	var body = document.body;
+	body.style.backgroundColor = "#DBFFFF";
+
+	var headerText = document.getElementsByTagName('h1')[0];
+	headerText.innerHTML = "Karas Emotions - (title updated via JavaScript bitch!)";
+
+
+
 alert('testing some alert!');
