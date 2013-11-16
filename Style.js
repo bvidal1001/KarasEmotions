cb443bfea2e6a666670c1d2dbda7a822355432d6
bvidal1001 <bradjvidal@yahoo.com>
2013-11-16 10:08:28 -0800

    Create Style Page

---
+++ Style.js
@@ -0,0 +1,16 @@
+
+	//Header Text
+	var headerText = document.getElementsByTagName('h1')[0];
+	headerText.innerHTML = "Karas Emotions - (title updated via JavaScript bitch!)";
+
+	//Header Color
+	var header = document.getElementsByTagName('header')[0];
+	header.style.backgroundColor = "#FFCCFF";
+
+	//Background Color
+	var body = document.body;
+	body.style.backgroundColor = "#DBFFFF";
+
+	
+	
+	
