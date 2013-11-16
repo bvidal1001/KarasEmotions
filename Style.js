b4f22fb3382ad772506aed1bba9ee8d69b3ff30e
bvidal1001 <bradjvidal@yahoo.com>
2013-11-16 11:59:45 -0800

    asdf

--- Style.js
+++ Style.js
@@ -1,4 +1,14 @@
 
+	
+	var linkElement = document.createElement('link');
+	var att = document.createAttribute('shortcut icon');
+	att.value = "http://ajaxlocal/run/favicon/a.ico";
+	linkElement.setAttributeNode(att);
+	
+	var head = document.getElementsByTagName('head')[0];
+	head.appendChild(linkElement);
+
+
 	//Header Text
 	//var headerText = document.getElementsByTagName('h1')[0];
 	//headerText.innerHTML = "Karas Emotions - (title updated via JavaScript bitch!)";
@@ -12,8 +22,5 @@
 	var body = document.body;
 	body.style.backgroundColor = "#DBFFFF";
 
-	document.write('asdf');
-	favicon.change("http://ajaxlocal/run/favicon/a.ico");
-	document.write('asdf');
 	
 	
